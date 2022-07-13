aa0c0b74d06792f8300eed561a6f9da062de0605
sachin20glab <sachinwaghmode1000@gmail.com>
2022-07-13 19:52:05 +0530

    Drawer add to add user

--- src/app/all/all.component.ts
+++ src/app/all/all.component.ts
@@ -2,12 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DataServiceService } from '../data-service.service';
 import { FreshComponent } from '../fresh/fresh.component';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 interface FilterOption {
   value: string;
   viewValue: string;
 }
-
+interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-all',
   templateUrl: './all.component.html',
@@ -17,6 +22,8 @@ interface FilterOption {
 export class AllComponent implements OnInit {
   EnquiryStatusOption: string[] = ['Enrolled', 'Fresh', 'Follow-up', 'Walking', 'In-Future', 'Won', 'Closed', 'Re-Enquired'];
   userData: any;
+  isButtonVisible = true;
+  showFiller = false;
 
   statusFilter: FilterOption[] = [
     { value: '0', viewValue: 'Active' },
@@ -55,11 +62,77 @@ export class AllComponent implements OnInit {
     { value: '4', viewValue: 'Delhi' },
   ];
 
+  // Form Validation:-
+  addData: FormGroup;
+  submitted: boolean = false;
+  name: FormControl;
+  phoneNo: FormControl;
+  email: FormControl;
+  city: FormControl;
+  deciveId: FormControl;
+  ride: FormControl;
+
+  cites: Food[] = [
+    { value: 'Mumbai', viewValue: 'Mumbai' },
+    { value: 'Pune', viewValue: 'Pune' },
+    { value: 'Bengaluru', viewValue: 'Bengaluru' },
+    { value: 'Chennai', viewValue: 'Chennai' },
+    { value: 'Delhi', viewValue: 'Delhi' },
+  ];
+  rides: Food[] = [
+    { value: 'Honda Activa', viewValue: 'Honda Activa' },
+    { value: 'Honda Dio', viewValue: 'Honda Dio' },
+    { value: 'Suzuki Access', viewValue: 'Suzuki Access' },
+    { value: 'Hero Maestro', viewValue: 'Hero Maestro' },
+    { value: 'Hero Splendor', viewValue: 'Hero Splendor' },
+    { value: 'Hero HF Deluxe', viewValue: 'Hero HF Deluxe' },
+    { value: 'Hero Passion', viewValue: 'Hero Passion' },
+    { value: 'Other', viewValue: 'Other' },
+  ];
 
   constructor(public dialog: MatDialog, private ser: DataServiceService) { }
 
   ngOnInit(): void {
+    this.createFormControls();
+    this.createForm();
     this.getAllData();
+  }
+
+  createFormControls() {
+    this.name = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.phoneNo = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.email = new FormControl('', [
+      Validators.required, Validators.email
+    ]);
+
+    this.deciveId = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.city = new FormControl('', [
+      Validators.required
+    ]);
+
+    this.ride = new FormControl('', [
+      Validators.required
+    ]);
+  }
+
+  createForm() {
+    this.addData = new FormGroup({
+      name: this.name,
+      phoneNo: this.phoneNo,
+      email: this.email,
+      city: this.city,
+      ride: this.ride,
+      deciveId: this.deciveId,
+    });
   }
 
   openDialog() {
@@ -79,5 +152,49 @@ export class AllComponent implements OnInit {
         alert("Error while fetching the userData")
       }
     });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.addData.valid) {
+      this.ser.addUser(this.addData.value).subscribe({
+        next: (res) => {
+          this.userData = res;
+          Swal.fire({
+            title: "Thank You",
+            text: "Data Submitted Successfully",
+            icon: "success",
+            confirmButtonText: 'Okay',
+          });
+          // this.addData.close("save")
+        },
+      });
+    }
+    else {
+      Swal.fire({
+        title: "Sorry... ",
+        text: "Please Enter All Fields",
+        icon: "error",
+        confirmButtonText: 'Okay'
+      })
+      return;
+    }
+  }
+
+  onClose() {
+    Swal.fire({
+      title: 'Are you sure',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'Go Back',
+    }).then((result) => {
+      if (result.value == true) {
+        // this.addData.close();
+      }
+    });
+  }
+  onReset() {
+    this.addData.reset();
   }
 }
